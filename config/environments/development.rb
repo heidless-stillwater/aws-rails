d9eed9d72d092ac77cb178189922efcf01ffba69
heidless <rob.lockhart@yahoo.co.uk>
2024-07-20 14:48:41 +0100

    deveopment.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,32 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { 
+    :host => 'localhost', 
+    :port => 3000 
+  }
+  config.action_mailer.delivery_method = :smtp
+
+
+  config.action_mailer.smtp_settings = {
+#    :address => 'smtp.sendgrid.net',
+    :address => Rails.application.credentials.aws[:ses_server],
+    :port => 587,   # 465	(for SSL connections). 25, 587	(for unencrypted/TLS connections)
+    :user_name => Rails.application.credentials.aws[:ses_username],
+    :password => Rails.application.credentials.aws[:ses_password],
+    :domain => Rails.application.credentials.aws[:ses_domain], 
+    :authentication => :login,
+    :enable_starttls_auto => true
+  }
+
+
+
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
@@ -32,9 +58,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
