09502a9c9fcb1e234f65215f66d6c319c53a88a6
heidless <rob.lockhart@yahoo.co.uk>
2024-08-19 09:51:31 +0100

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = [
+  { email: 'lockhart.r@gmail.com', first_name: 'rob', last_name: 'sysadmin', admin: true, bio: 'Hey!' },
+  { email: 'lockhartarts.co.uk@domainsbyproxy.com', first_name: 'admin', last_name: 'sysadmin', admin: true, bio: 'Hey!' },
+  { email: 'rob@test.com', first_name: 'rob', last_name: 'sysadmin', admin: true, bio: 'Hey!' },
+  { email: 'bear@test.com', first_name: 'bear', last_name: 'woods', admin: false, bio: 'Hey!' },  
+
+]
+
+users.each do |user|
+  this_user = User.where(
+    email: user[:email],
+  ).first_or_initialize
+
+  this_user.update!(
+    first_name: user[:first_name],
+    last_name: user[:last_name],
+    admin: user[:admin],
+    bio: user[:bio],
+    password: SecureRandom.uuid,
+  )
+  this_user.save
+end
